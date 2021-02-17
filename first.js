df2acb9ea38897b4f7ac9757387c59ba619f1835
Volodymyr Himiak <vv.himiak@gmail.com>
2021-02-17 07:08:52 +0200

    4-th commit

--- first.js
+++ first.js
@@ -3,3 +3,21 @@ console.log('hello');
 // letters, numbers, _, $
 // can not start with a number
 console.log('hellp');
+let num = String(555);
+
+console.log(num);
+console.log(typeof num);
+
+let arr = new Array(5,6,7);
+
+console.log(arr); 
+
+const companies = ['fa','gg'];
+
+for(let i = 0; i < companies.length; i++) {
+ console.log(companies[i]);
+}
+
+companies.forEach(function(co, iv, ar){
+ return console.log(co[iv], ar)
+})
